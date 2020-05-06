508561779fe5b7fe1f741d0b33bf53dc47ef6d48
Vivian LEE <htleeab@gmail.com>
2020-05-06 12:00:33 +0800

    video generation: add options for 2 stage, clean, resolution; don't generate other image in folder, improve processbar tqdm UI

--- codes/generate_video.py
+++ codes/generate_video.py
@@ -1,4 +1,7 @@
 '''
+This script is used for generate video or images sequence from video
+please install required pretrained models under experiments/pretrained_models
+
 reference: https://github.com/btahir/deoldify_and_edvr/blob/master/DeOldify_EDVR_Combined.ipynb
 modification: Vivian LEE
 Possible improvement: frame-by-frame without writing frame to disk, multi-GPU support
@@ -72,13 +75,28 @@ def purge_images(dir):
             os.remove(os.path.join(dir, f))
 
 
-def extract_raw_frames(source_path: Path):
+def get_frame_count(video_filepath):
+    vid_in = cv2.VideoCapture(str(video_filepath))
+    frame_count = int(vid_in.get(cv2.CAP_PROP_FRAME_COUNT))
+    vid_in.release()
+    return frame_count
+
+
+def extract_raw_frames(source_path: Path, resolution: tuple):
     inframes_folder = inframes_root / (source_path.stem)
     inframes_folder.mkdir(parents=True, exist_ok=True)
     inframe_path_template = str(inframes_folder / ('%5d.' + img_format))
-    purge_images(inframes_folder)
-    ffmpeg.input(str(source_path)).output(str(inframe_path_template), format='image2',
-                                          vcodec=img_vcodec, qscale=0).run(capture_stdout=True)
+    if len(os.listdir(inframes_folder)) == get_frame_count(source_path):
+        print(f'frame of {source_path} has been extracted already, skip')
+    else:
+        print(
+            f'{inframes_folder}: {len(os.listdir(inframes_folder))} == {get_frame_count(source_path)}'
+        )
+        purge_images(inframes_folder)
+        resolution_str = ':'.join([str(x) for x in resolution])
+        subprocess.call(
+            f'ffmpeg -y -i {str(source_path)} -s {resolution_str} -f image2'
+            f' -pix_fmt rgb24 -c:v {img_vcodec} {inframe_path_template}', shell=True)
     return inframes_folder
 
 
@@ -158,29 +176,21 @@ def edvrPredict(data_mode, stage, chunk_size, test_dataset_folder, save_folder):
     preProcess_multiple_factor = 16 if predeblur else 4
     padding = 'new_info' if data_mode in ('Vid4',
                                           'sharp_bicubic') else 'replicate'  # temporal padding mode
-    save_imgs = True
 
     util.mkdirs(save_folder)
-    subfolder_name_l = []
-    # remove old video_subfolder if exists
-    remove_subfolders()
-    subfolder_l = sorted(glob.glob(osp.join(test_dataset_folder, '*')))
+    remove_subfolders()  # remove old video_subfolder if exists
 
-    # for each subfolder
-    for subfolder in subfolder_l:
-        subfolder_name = osp.basename(subfolder)
-        subfolder_name_l.append(subfolder_name)
-        save_subfolder = osp.join(save_folder, subfolder_name)
+    img_path_l = sorted(glob.glob(osp.join(test_dataset_folder, '*')))
+    util.mkdirs(save_folder)
+    purge_images(save_folder)
 
-        img_path_l = sorted(glob.glob(osp.join(subfolder, '*')))
-        if save_imgs:
-            util.mkdirs(save_subfolder)
-            purge_images(save_subfolder)
+    preProcess(img_path_l, preProcess_multiple_factor)
+    # probably do not have enough memory to load all frames, parse video frames into subfolders for loading frame with read_img_seq function
+    # TODO: pass list of image to data_util.read_img_seq instead of using subfolder
+    subFolderList = make_subfolders(img_path_l, chunk_size)
 
-        preProcess(img_path_l, preProcess_multiple_factor)
-        # make even more subfolders
-        subFolderList = make_subfolders(img_path_l, chunk_size)
-
+    with tqdm(total=len(img_path_l)) as pbar:
+        frame_num = 0
         #### read images in chunks, clean memory after each chunk
         for subSubFolder in subFolderList:
             clean_mem()
@@ -189,7 +199,10 @@ def edvrPredict(data_mode, stage, chunk_size, test_dataset_folder, save_folder):
             max_idx = len(subSubFolder_l)
 
             # process each image
-            for img_idx, img_path in tqdm(enumerate(subSubFolder_l)):
+            for img_idx, img_path in enumerate(subSubFolder_l):
+                with open('offset_mean.log', 'a') as file:
+                    file.write(f'[generate_video.py] forwarding frame {frame_num}\n')
+
                 img_name = osp.splitext(osp.basename(img_path))[0]
                 select_idx = data_util.index_generation(img_idx, max_idx, N_in, padding=padding)
                 imgs_in = imgs_LQ.index_select(0,
@@ -198,14 +211,14 @@ def edvrPredict(data_mode, stage, chunk_size, test_dataset_folder, save_folder):
                 output = util.single_forward(model, imgs_in)
                 output = util.tensor2img(output.squeeze(0))
 
-                if save_imgs:
-                    cv2.imwrite(osp.join(save_subfolder, '{}.{}'.format(img_name, img_format)),
-                                output)
+                cv2.imwrite(osp.join(save_folder, '{}.{}'.format(img_name, img_format)), output)
+                pbar.update()
+                frame_num += 1
+
     return save_folder
 
 
-def encode_video(source_path: Path, outframes_root: Path, result_path: Path) -> Path:
-    outframes_folder = outframes_root / (source_path.stem)
+def encode_video(source_path: Path, outframes_folder: Path, result_path: Path) -> Path:
     outframes_path_template = str(outframes_folder / ('%5d.' + img_format))
     result_folder.mkdir(parents=True, exist_ok=True)
     fps = get_fps(source_path)
@@ -222,51 +235,129 @@ def encode_video(source_path: Path, outframes_root: Path, result_path: Path) -> 
                     shell=True)
 
     # encode output
-    if audio_file.exists:
+    if audio_file.exists and os.path.exists(audio_file):
         # combine images and audio
         subprocess.call(
             f'ffmpeg -y -f image2 -r {fps} -c:v {img_vcodec} -i "{outframes_path_template}"'
-            f'-i "{str(audio_file)}" -c:v copy -c:a copy "{str(result_path)}"',
-            shell=True)
+            f' -i "{str(audio_file)}" -c:a copy '
+            f' -c:v libx264 -b:v 3.2M "{str(result_path)}"', shell=True)
         audio_file.unlink()
     else:
         subprocess.call(
             f'ffmpeg -y -f image2 -r {fps} -c:v {img_vcodec} -i "{outframes_path_template}"'
-            f' -c:v libx264 -vrf 17 "{str(result_path)}"',
-            shell=True)
+            f' -c:v libx264 -b:v 3.2M "{str(result_path)}"', shell=True)
 
     return result_path
 
 
-def edvr_video(source_path: Path, data_mode: str, chunk_size: int, finetune_stage2: bool):
-    # extract frames
-    extract_raw_frames(source_path)
+def encode_images(outframes_folder, output_filepath, fps):
+    outframes_path_template = str(outframes_folder / ('%5d.' + img_format))
+    result_folder = Path(os.path.dirname(output_filepath))
+    result_folder.mkdir(parents=True, exist_ok=True)
+    subprocess.call(
+        f'ffmpeg -y -f image2 -r {fps} -c:v {img_vcodec} -i "{outframes_path_template}"'
+        f' -c:v libx264 -vrf 17 "{str(output_filepath)}"', shell=True)
+    return output_filepath
+
+
+def edvr_img2video(img_src_dir: Path, data_mode: str, chunk_size: int, finetune_stage2: bool,
+                   clean_frames: bool):
+    sub_folder_name = f'{img_src_dir.stem}_{data_mode}'
 
     # process frames
-    outframes = edvrPredict(data_mode, 1, chunk_size, inframes_root, outframes_s1_root)
+    outframes = edvrPredict(data_mode, 1, chunk_size, img_src_dir,
+                            outframes_s1_root / sub_folder_name)
 
     # fine-tune stage 2
     if finetune_stage2:
         print(f'fine-tune with stage 2 model')
-        outframes = edvrPredict(data_mode, 2, chunk_size, outframes_s1_root, outframes_s2_root)
+        outframes = edvrPredict(data_mode, 2, chunk_size, outframes,
+                                outframes_s2_root / sub_folder_name)
 
     # Encode video from predicted frames
-    output_video_path = result_folder / f'{source_path.stem}_{data_mode}.mp4'
-    encode_video(source_path, outframes, output_video_path)
+    output_video_path = result_folder / f'{img_src_dir.name}_{data_mode}.mp4'
+    encode_images(outframes, output_video_path, 25)
 
     print(f'Video output: {output_video_path}')
+
+    if clean_frames:
+        shutil.rmtree(inframes_root, ignore_errors=True, onerror=None)
+        shutil.rmtree(outframes_s1_root, ignore_errors=True, onerror=None)
+        shutil.rmtree(outframes_s2_root, ignore_errors=True, onerror=None)
+
+
+def edvr_video(video_src_path: Path, data_mode: str, chunk_size: int, finetune_stage2: bool,
+               clean_frames: bool, resolution: tuple):
+    sub_folder_name = f'{video_src_path.stem}_{data_mode}'
+
+    # extract frames
+    inframes_folder = extract_raw_frames(video_src_path, resolution)
+
+    # process frames
+    outframes = edvrPredict(data_mode, 1, chunk_size, inframes_folder,
+                            outframes_s1_root / sub_folder_name)
+
+    # fine-tune stage 2
+    if finetune_stage2:
+        print(f'fine-tune with stage 2 model')
+        outframes = edvrPredict(data_mode, 2, chunk_size, outframes,
+                                outframes_s2_root / sub_folder_name)
+
+    # Encode video from predicted frames
+    output_video_path = result_folder / f'{video_src_path.stem}_{data_mode}.mp4'
+    encode_video(video_src_path, outframes, output_video_path)
+
+    print(f'Video output: {output_video_path}')
+
+    if clean_frames:
+        shutil.rmtree(inframes_root, ignore_errors=True, onerror=None)
+        shutil.rmtree(outframes_s1_root, ignore_errors=True, onerror=None)
+        shutil.rmtree(outframes_s2_root, ignore_errors=True, onerror=None)
+
+
+def str2bool(v):
+    # ref: https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+def str2tuple(v):
+    if isinstance(v, tuple):
+        return v
+    try:
+        return tuple(v.split(','))
+    except:
+        raise argparse.ArgumentTypeError(f'cannot turn {v} into tuple')
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', dest='input', type=str, default='../video/input.mp4')
+    parser.add_argument('-i', '--input', dest='input', default='../video/input.mp4',
+                        help='input video file or folder of image sequences')
     parser.add_argument('-m', '--model', dest='model', type=str, default='blur_comp',
                         choices=['Vid4', 'sharp_bicubic', 'blur_bicubic', 'blur', 'blur_comp'])
+    parser.add_argument('-s', '--two-stage', dest='two_stage_enabled', type=str2bool, default=True)
+    parser.add_argument('-c', '--clean', dest='clean_frames', type=str2bool, default=True)
+    parser.add_argument('-r', '--resolution', dest='resolution', type=str2tuple,
+                        default=(1280, 720))
     args = parser.parse_args()
+    if args.model == 'Vid4':
+        assert not args.two_stage_enabled, f'Vid4 do not have stage 2 pretrained model'
     return args
 
 
 if __name__ == '__main__':
+    if os.path.exists('offset_mean.log'):
+        os.remove('offset_mean.log')
     args = parse_args()
-    enable_finetune = args.model != 'Vid4'  # Vid4 do not have stage 2 pretrained model
-    edvr_video(Path(args.input), args.model, 100, enable_finetune)
+    if not os.path.isdir(args.input):
+        edvr_video(Path(args.input), args.model, 100, args.two_stage_enabled, args.clean_frames,
+                   args.resolution)
+    else:
+        edvr_img2vid(args.input, args.model, 100, args.two_stage_enabled, args.clean_frames)
